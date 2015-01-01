d3be4be9e199e72dffdc7e09da15387b04a313b2
Jorge D. Ortiz-Fuentes <jdortiz@gmail.com>
2015-01-01 19:47:57 +0000

    Show a results activity when the user presses the button.

--- app/src/main/java/com/powwau/challenge13/MainActivity.java
+++ app/src/main/java/com/powwau/challenge13/MainActivity.java
@@ -1,11 +1,13 @@
 package com.powwau.challenge13;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -138,6 +140,13 @@ public class MainActivity extends ActionBarActivity {
 
     private void prepareButton() {
         mButton = (Button)findViewById(R.id.button_show_me);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ResultActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
