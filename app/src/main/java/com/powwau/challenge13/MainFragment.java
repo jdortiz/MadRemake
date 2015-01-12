3001da00f78a1ee0844535999e5398342be3f5c5
Jorge D. Ortiz-Fuentes <jdortiz@gmail.com>
2015-01-12 01:19:43 +0100

    Inflate the expected fragment layout in the MainFragment class.

--- app/src/main/java/com/powwau/challenge13/MainFragment.java
+++ app/src/main/java/com/powwau/challenge13/MainFragment.java
@@ -6,7 +6,6 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 
 /**
@@ -23,9 +22,7 @@ public class MainFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        TextView textView = new TextView(getActivity());
-        textView.setText(R.string.hello_blank_fragment);
-        return textView;
+        return inflater.inflate(R.layout.fragment_main, container, false);
     }
 
 
